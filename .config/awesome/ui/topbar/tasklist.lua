5a315b3ff5323fff6a9b79ac6a749ca4e08b3ebd
Angel Andrade <aandradeb99@gmail.com>
2023-08-07 18:28:41 +0200

    style: indent tasklist button function

--- .config/awesome/ui/topbar/tasklist.lua
+++ .config/awesome/ui/topbar/tasklist.lua
@@ -6,26 +6,26 @@ local beautiful = require("beautiful")
 
 function get_tasklist(s)
 	local tasklist_buttons = gears.table.join(
-		awful.button({ }, 1, function (c)
-								if c == client.focus then
-									c.minimized = true
-								else
-									c:emit_signal(
-										"request::activate",
-										"tasklist",
-										{raise = true}
-									)
-								end
-							end),
-		awful.button({ }, 3, function()
-								awful.menu.client_list({ theme = { width = 250 } })
-							end),
-		awful.button({ }, 4, function ()
-								awful.client.focus.byidx(1)
-							end),
-		awful.button({ }, 5, function ()
-								awful.client.focus.byidx(-1)
-							end))
+		awful.button({}, 1, function(c)
+			if c == client.focus then
+				c.minimized = true
+			else
+				c:emit_signal(
+					"request::activate",
+					"tasklist",
+					{ raise = true }
+				)
+			end
+		end),
+		awful.button({}, 3, function()
+			awful.menu.client_list({ theme = { width = 250 } })
+		end),
+		awful.button({}, 4, function()
+			awful.client.focus.byidx(1)
+		end),
+		awful.button({}, 5, function()
+			awful.client.focus.byidx(-1)
+		end))
 
 	local mytasklist = awful.widget.tasklist {
 		screen = s,
