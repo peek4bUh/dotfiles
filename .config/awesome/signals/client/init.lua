e967694eba0b8f02950b01bc3f6c0a85aeeb56b4
Angel Andrade <aandradeb99@gmail.com>
2024-05-09 13:49:20 +0200

    Remove mouse signal when entering in windows without click

--- .config/awesome/signals/client/init.lua
+++ .config/awesome/signals/client/init.lua
@@ -2,10 +2,6 @@ local awful = require("awful")
 require("awful.autofocus")
 local wibox = require("wibox")
 
-client.connect_signal("mouse::enter", function(c)
-	c:activate({ context = "mouse_enter", raise = false })
-end)
-
 client.connect_signal("request::titlebars", function(c)
 	-- buttons for the titlebar
 	local buttons = {
