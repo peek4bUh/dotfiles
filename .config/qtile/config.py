f5ee0744af63781a65a00ad5197f984c93ce03ac
Angel Andrade <aandradeb99@gmail.com>
2024-06-08 19:16:54 +0200

    Change height and bgcolor of bar

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -165,7 +165,8 @@ screens = [
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
                 widget.QuickExit(),
             ],
-            24,
+            48,
+            background="#282a36",
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
